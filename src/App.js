3cef8fff2bd12dac40cb56a427ece4348f56d043
sonsumin <sonsumin00@gmail.com>
2024-10-17 12:27:50 +0900

    Feat: State를 통한 리랜더링

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css";
 
 export default class App extends Component {
 
+    state = {
+        todoData : [
+            {
+                id: 1,
+                title: "공부하기",
+                completed: true,
+            },
+            {
+                id: 2,
+                title: "청소하기",
+                completed: false
+            }
+        ]
+    }
+
     btnStyle = {
         color: "#fff",
         border: "none",
@@ -20,18 +35,11 @@ export default class App extends Component {
         }
     }
 
-    todoData = [
-        {
-            id: 1,
-            title: "공부하기",
-            completed: true,
-        },
-        {
-            id: 2,
-            title: "청소하기",
-            completed: false
-        }
-    ]
+    handleClick = (id) => {
+        let newTodoData = this.state.todoData.filter(data => data.id !== id)
+        console.log('newTodoData', newTodoData)
+        this.setState({todoData: newTodoData})
+}
 
   render() {
       return (
@@ -41,11 +49,11 @@ export default class App extends Component {
                       <h1>할 일 목록</h1>
                   </div>
 
-                  {this.todoData.map((data) => (
+                  {this.state.todoData.map((data) => (
                       <div style={this.getStyle()} key={data.id}>
                           <input type="checkbox" defaultChecked={false}/>
                           {data.title}
-                          <button style={this.btnStyle}>x</button>
+                          <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
                       </div>
                   ))}
 
